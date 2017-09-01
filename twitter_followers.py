e8310c6ac21cf04be5935c9c4b699c7b3be5e6a0
Sarai <anna.sarai.rosenberg@gmail.com>
2017-08-31 19:36:52 -0700

    fix unfollow, revise upsert

--- twitter_followers.py
+++ twitter_followers.py
@@ -14,7 +14,7 @@ auth.set_access_token(tokens.access_token, tokens.access_token_secret)
 api = tweepy.API(auth)
 
 date = datetime.datetime.now()
-user = "techieslikeus"
+user = "saraislet"
 my_id = api.get_user(user).id
 followers = []
 
@@ -33,7 +33,6 @@ def limit_handled(cursor):
 #    followers.append(follower)
 
 
-
 for follower in limit_handled(tweepy.Cursor(api.followers, user, count=100).items()):
     followers.append(follower)
 
@@ -41,7 +40,7 @@ for follower in limit_handled(tweepy.Cursor(api.followers, user, count=100).item
 db = sqlite3.connect('followers.db')
 cursor = db.cursor()
 
-# Build a list of followers from db.
+# OLD: Build a list of followers from db.
 #followers = []
 #db.execute('''SELECT id, follower_id FROM followers WHERE twitter_id = my_id''')
 #for row in db:
@@ -52,24 +51,38 @@ cursor = db.cursor()
 userList = []
 for user in followers:
     try:
-        # Try to update the follower for my_id matching user.id
-        cursor.execute('''UPDATE followers SET date_checked = ? WHERE twitter_id = ? AND follower_id = ?''', (date, my_id, user.id))
-        print("Updated: " + str(my_id) + ", " + str(user.id) + ", " + user.screen_name + ", " + str(date))
-        db.commit()
+        # Try to find the follower for my_id matching user.id. (This should be unique.)
+        # TODO: Is it necessary to update every record to detect unfollowers?
+        # TODO: Consider testing different options to find a more efficient solution.
+        cursor.execute('''SELECT id, following, date_unfollowed FROM followers WHERE twitter_id = ? AND follower_id = ?''', (my_id, user.id))
+        row = cursor.fetchone()
+        
+        # If there is no follower in db, create a new row.
+        if row is None:
+             cursor.execute('''INSERT INTO followers(twitter_id, follower_id, screen_name, date_added, date_checked, following)
+                  VALUES(?,?,?,?,?,1)''', (my_id, user.id, user.screen_name, date, date))  
+             print("Inserted: " + str(my_id) + ", " + str(user.id) + ", " + user.screen_name + ", " + str(date))
+             db.commit()
+        # If there is a follower, update the date checked, and set following to 1.
+        else:
+            cursor.execute('''UPDATE followers SET date_checked = ?, following = 1 WHERE twitter_id = ? AND follower_id = ?''', (date, my_id, user.id))
+            print("Updated: " + str(my_id) + ", " + str(user.id) + ", " + user.screen_name + ", " + str(date))
+            db.commit()
     except Exception as e:
-        # If that user.id does not exist, create a new row in followers.
-        cursor.execute('''INSERT INTO followers(twitter_id, follower_id, screen_name, date_added)
-                  VALUES(?,?,?,?)''', (my_id, user.id, user.screen_name, date))  
-        print("Inserted " + str(my_id) + ", " + str(user.id) + ", " + user.screen_name + ", " + str(date))
-        db.commit()
+        print("Error: " + str(e))
+        # OLD: If that user.id does not exist, create a new row in followers.
+        #cursor.execute('''INSERT INTO followers(twitter_id, follower_id, screen_name, date_added, following)
+        #          VALUES(?,?,?,?,?)''', (my_id, user.id, user.screen_name, date, 1))  
+        #print("Inserted " + str(my_id) + ", " + str(user.id) + ", " + user.screen_name + ", " + str(date))
+        #db.commit()
 
-# Check for followers in the db with null date_unfollowed not listed in the retrieved followers.
+# Check for followers in the db with null date_unfollowed where date_checked is earlier than date.
 # These are unfollowers.
-cursor.execute('''SELECT id, follower_id, screen_name FROM followers WHERE twitter_id = ? AND follower_id = ? AND date_unfollowed IS NULL''', (my_id, user.id))
+cursor.execute('''SELECT id, screen_name FROM followers WHERE twitter_id = ? AND following = 1 AND date_checked < ?''', (my_id, date))
 rows = cursor.fetchall()
 for row in rows:
-    cursor.execute('''UPDATE followers SET date_unfollowed = ? WHERE id = ? ''', (date, row[0]))
-    print("Unfollower: " + row[2])
+    cursor.execute('''UPDATE followers SET date_unfollowed = ?, following = 0 WHERE id = ? ''', (date, row[0]))
+    print("Unfollower: " + row[1])
 
 # Commit the above changes to the db, and close the db connection.
 db.commit()
@@ -78,14 +91,13 @@ db.close()
 # Quick function to grab a list of IDs.
 # followers = tweepy.API.followers_ids("saraislet")
 
-# Print followers to file followers.txt, 
+# OLD (bug testing) Print followers to file followers.txt, 
 # encoding the list as a string with utf-8 characters
 
 # fh = open('followers.txt', 'w')
 # fh.write(str(str(followers).encode('utf-8')))
 # fh.close()
 
+# TODO: Log to file instead of printing to console.
 
-#print(followers)
-#print(userList)
 print("Done.")
