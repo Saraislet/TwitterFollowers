287bfece9140e48b5a0c2a684edccc0ab2bd11fd
Sarai <anna.sarai.rosenberg@gmail.com>
2017-09-17 13:04:31 -0700

    test heroku

--- test.py
+++ test.py
@@ -5,7 +5,12 @@ Created on Sun Sep 17 13:01:01 2017
 @author: Sarai
 """
 
-from flask import Flask
+#from flask import Flask
+from flask import Flask, request
+import flask
+import tweepy
+import tokens
+
 
 app = Flask(__name__)
 
